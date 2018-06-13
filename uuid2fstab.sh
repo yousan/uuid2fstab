f33fcc560b9b45bcb7f5286483c6c1c61b4388e8
Yousan_O <yousan@l2tp.org>
2018-06-13 22:39:18 +0900

    Create uuid2fstab.sh

---
+++ uuid2fstab.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# blkid to fstab format
+# outputs
+# UUID=1234-37bd-4098-9f8b-85e99c9c5219 /boot                   xfs     defaults        0 0
+blkid | grep $1 | sed -r 's#^.*UUID="([a-Z0-9\-]+)".*TYPE="([a-Z0-9\-]+)".*$#UUID=\1\t'$2'\t\2\tdefaults\t0\t0#'
